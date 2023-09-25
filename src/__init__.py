05ead3c5142a45d1adba40f34634c4be82fc3720
jkomijani <jkomijani@gmail.com>
2023-09-25 10:13:01 +0200

    _svd/svd.py splitted to _svd/svd_general.py & _svd/svd_2x2.py; _svd/__init__.py introduced.

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@
 # functions from _autograd support algorithmic differentiation
 from ._autograd import eigh, eigu
 
-from ._svd.svd import svd
+from ._svd import svd
--- src/_svd/svd_general.py
+++ src/_svd/svd_general.py
@@ -135,20 +135,3 @@ def append_suvd(svd):
     # we now make determinant of uvh unity:
     uvh = uvh / rdet.reshape(*rdet.shape, 1, 1)
     return AttributeDict(U=svd.U, S=svd.S, Vh=svd.Vh, rdet_uvh=rdet, sUVh=uvh)
-
-
-def svd_for_su2sums(matrix):
-    """
-    Special case where x is a sum of SU(2) matrices, for which one can show
-    x.adjoint() @ x is proportional to the identity matrix.
-    """
-    s = (torch.abs(torch.linalg.det(matrix))**0.5).unsqueeze(-1)
-    u = matrix / s.unsqueeze(-1)
-    vh = torch.zeros_like(u)
-    vh[..., 0, 0] = 1.
-    vh[..., 1, 1] = 1.
-    uvh = u @ vh
-    det_uvh = torch.det(uvh)
-    uvh[..., 0] = uvh[..., 0] / det_uvh.unsqueeze(-1)  # change only 1st column
-    s = torch.cat([s, s], dim=-1)
-    return AttributeDict(U=u, S=s, Vh=vh, det_uvh=det_uvh, sUVh=uvh)
